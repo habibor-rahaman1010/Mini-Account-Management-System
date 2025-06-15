1704bac5e40605cbe7510cd7341f907c14aa610e
habibor-rahaman1010 <habibor144369@gmail.com>
2025-06-15 14:16:37 +0600

    User Profile Feature complete

---
+++ Mini.Account.Management.System/Account.Management.Web/Areas/Admin/Controllers/AccountManagement/UserProfileController.cs
@@ -0,0 +1,39 @@
+﻿using Account.Management.Infrastructure.Account.Management.Identity;
+using Account.Management.Web.Areas.Admin.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Account.Management.Web.Areas.Admin.Controllers.AccountManagement
+{
+    [Area("Admin"), Authorize]
+    public class UserProfileController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserProfileController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [Authorize(Roles = "Admin, Accountant, Viewer")]
+        public async Task<IActionResult> Profile()
+        {
+            var currentUser = await _userManager.GetUserAsync(User);
+            if (currentUser == null)
+            {
+                return NotFound();
+            }
+
+            var roles = await _userManager.GetRolesAsync(currentUser);
+
+            var userViewModel = new UserProfileViewModel
+            {
+                User = currentUser,
+                Roles = roles
+            };
+
+            return View(userViewModel);
+        }
+    }
+}
---
+++ Mini.Account.Management.System/Account.Management.Web/Areas/Admin/Models/UserProfileViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Account.Management.Infrastructure.Account.Management.Identity;
+
+namespace Account.Management.Web.Areas.Admin.Models
+{
+    public class UserProfileViewModel
+    {
+        public ApplicationUser User { get; set; }
+        public IList<string> Roles { get; set; }
+
+        public UserProfileViewModel()
+        {
+            User = new ApplicationUser();
+            Roles = new List<string>();
+        }
+    }
+}
